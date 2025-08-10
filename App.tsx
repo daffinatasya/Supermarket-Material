781c3990cd1ca23653ae13cb756b28f9d11947f9
daffinatasya <daffinatasya@gmail.com>
2025-08-10 20:31:52 +0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MaterialManagementApp } from './components/MaterialManagementApp';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <MaterialManagementApp />
+    </div>
+  );
+}
+
+export default App;
